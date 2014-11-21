8b60f51b74c33b1c187bc600684ee1e86df6f2f6
Memo Lopez <tr3intaydos@gmail.com>
2014-11-20 22:19:16 -0600

    First working protoype
    Just parsing and visual, needs more works and info for each star.

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Star{
+
+	public string starName;
+	public float distance;
+	public Vector3 position;
+	public float absoluteMagnitude;
+	
+	private float _relativeMagnitude = -1.0f;
+	
+	public float relativeMagnitude{
+		get {
+			if(_relativeMagnitude == -1.0f){
+				_relativeMagnitude = 5 * (Mathf.Log10(distance) - 1) + absoluteMagnitude;
+			}
+		
+			return _relativeMagnitude;	
+		}
+	}
+}
---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StarManager : MonoBehaviour {
+
+	public TextAsset csvData;
+	public List<Star> starData = new List<Star>();
+	public int amountOfStars = 10000;
+	public GameObject prefabStar;
+
+	void Start () {
+		if(!csvData)
+			Debug.LogError("Insert csv file");
+			
+		ParseFile();
+		CreateStars();
+	}
+	
+	void ParseFile () {
+		string [] lines = csvData.text.Split('\n');
+		bool firstLineRead = false;
+		foreach(string line in lines){
+			if(!firstLineRead){
+				firstLineRead = true;
+				continue;
+			}
+			
+			string [] starLine = line.Split(',');
+			
+			Star star = new Star();
+			star.starName = starLine[3];
+			star.distance = float.Parse(starLine[12]);
+			star.position = new Vector3(float.Parse(starLine[13]), float.Parse(starLine[14]), float.Parse(starLine[15]));
+			star.absoluteMagnitude = float.Parse(starLine[16]);
+			
+			starData.Add(star);
+		}
+		
+		starData.Sort((a,b) => a.relativeMagnitude.CompareTo(b.relativeMagnitude));
+	}
+	
+	void CreateStars(){
+		int starsRendered = 0;
+		
+		while(starsRendered < amountOfStars){
+			GameObject star = Instantiate(prefabStar, starData[starsRendered].position, Quaternion.identity) as GameObject;
+			star.name = starData[starsRendered].starName;
+			Vector3 direction = star.transform.position - Camera.main.transform.position;
+			star.transform.rotation = Quaternion.LookRotation(direction);
+			starsRendered++;
+		}
+	
+	}
+}
